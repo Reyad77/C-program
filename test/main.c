8768fee45e813ab097212687365299699b6fa48d
Reyad07 <latif.tajnoor@gmail.com>
2023-10-13 00:37:57 +0800

    Format Specifier

--- test/main.c
+++ test/main.c
@@ -3,7 +3,16 @@
 
 int main()
 {
-    printf("Reyad!\n");
-    printf("Taizhou University");
-    return 0;
+    printf("Reyad\n");
+    printf("Taizhou University\n");
+
+    int num1= 56;
+    float num2=41.23;
+    char num3="x";
+    double num4=65.1215846847;
+    printf("Number1 is %lf \n",num4);
+    printf("Number 2 is %d \n",num1);
+    printf("Number 3 is %d \n",num2);
+    printf("Character is %c \n ",num3);
+    getch();
 }
