452df10ed5b82b71db1e257954aeabeffc97f631
Reyad07 <latif.tajnoor@gmail.com>
2023-10-13 01:09:25 +0800

    How to set number after point

--- test/main.c
+++ test/main.c
@@ -6,13 +6,18 @@ int main()
     printf("Reyad\n");
     printf("Taizhou University\n");
 
-    int num1= 56;
-    float num2=41.23;
-    char num3="x";
-    double num4=65.1215846847;
-    printf("Number1 is %lf \n",num4);
+    int num1= 56, num2=13;
+    float num3=41.23;
+    char num4='x';
+    double num5=65.1215846847;
+
+    printf("Number 1 is %.3lf \n",num5);
     printf("Number 2 is %d \n",num1);
-    printf("Number 3 is %d \n",num2);
-    printf("Character is %c \n ",num3);
+    printf("Number 3  is %.2f \n",num3);
+    printf("Numbers are %d, %d \n",num1, num2);
+    printf("Character is %c \n ",num4);
+    printf("Numbers are %d, %d, %.2f, %.3lf, %c \n",num1,num2,num3,num5,num4);
+
     getch();
 }
+
