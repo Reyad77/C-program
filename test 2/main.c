7c02a7831ec7aff33a5c9de8246433cefa7b86ea
Reyad07 <latif.tajnoor@gmail.com>
2023-10-20 10:55:50 +0800

    assigning values

--- test 2/main.c
+++ test 2/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    /* (int a = 10;
+    float x = 2.5;
+    char c= '*'; )
+
+   1. warning C4244:'=' : conversion form 'constdouble' to 'int', possible loss of data
+   2. erron C2065:'f' : undeclared identifier.*/
+     int a;
+     float f;
+      a=3;
+      f=1.2378;
+      printf("a=%d \n",a);
+      printf("f=%.2f \n ", f);
+      getch();
+
 }
