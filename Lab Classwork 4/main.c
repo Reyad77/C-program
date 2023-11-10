4d02ae9ffc5e129737ff75715b4ec65a70fac809
Reyad07 <latif.tajnoor@gmail.com>
2023-11-10 14:42:12 +0800

    Function

---
+++ Lab Classwork 4/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{float x,y;
+scanf("%f",&x);
+if(x<0)
+    if (x<=-10)
+    y=2*x;
+   else
+    y=2+x;
+else
+    if(x<=10)
+         y=x-2;
+    else
+         y=x/10;
+printf("\ny=%f",y);
+getch();
+}
