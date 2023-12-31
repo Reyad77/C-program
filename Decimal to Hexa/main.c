8015e100b0f03bde7577cd440baa2149ff3d2094
Reyad07 <latif.tajnoor@gmail.com>
2023-12-31 17:46:58 +0800

    Decimal to Hexa

---
+++ Decimal to Hexa/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int number;
+    printf("Enter a decimal number: ");
+    scanf("%d", &number);
+    printf("The Hexa-decimal number is %x", number);
+    getch();
+}
