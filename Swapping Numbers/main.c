c1ac3cf544212aeaa86425b344d4eeab1fdbe409
Reyad07 <latif.tajnoor@gmail.com>
2024-01-05 23:02:04 +0800

    Swapping numbers

---
+++ Swapping Numbers/main.c
@@ -0,0 +1,18 @@
+//Swapping Numbers//
+#include <stdio.h>
+int main()
+{
+   int num1=20;
+   int num2=30;
+   int temp;
+
+   temp=num1;
+   num1=num2;
+   num2=temp;
+
+   printf("First number is %d \n", num1);
+   printf("Second number is %d", num2);
+
+   getch();
+}
+
