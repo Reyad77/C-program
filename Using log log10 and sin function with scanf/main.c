a4d25cc4a8c94d1f62eb914690fc3a8bf65e48ba
Reyad07 <latif.tajnoor@gmail.com>
2024-01-07 15:56:58 +0800

    Using log 10, lod, sin function with scanf

---
+++ Using log log10 and sin function with scanf/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+    double x, result, result2,result3;
+    printf("Enter any number for x: ");
+    scanf("%lf",&x);
+    result = log(x);
+    result2=log10(x);
+    result3=sin(x);
+
+    printf("The result of log(%.2lf) = %.2lf. \n",x,result);
+    printf("The result of log10(%.2lf) = %.2lf. \n",x,result2);
+    printf("The result of sin(%.2lf) = %.2lf. \n",x,result3);
+    getch();
+
+}
