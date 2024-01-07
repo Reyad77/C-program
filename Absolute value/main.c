447cdf21a70f034daceff34a988f1e9594423e9f
Reyad07 <latif.tajnoor@gmail.com>
2024-01-07 15:16:09 +0800

    modified

--- Absolute value/main.c
+++ Absolute value/main.c
@@ -4,7 +4,7 @@ int main()
    int result,a;
    printf("Please enter any  integer number: ");
    scanf("%d",&a);
-   result =abs(-a);
+   result =abs(-a); // this code will also work without the minus (-)//
    printf("The absolute value is %d", result);
    getch();
 }
